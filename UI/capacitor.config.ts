b34bc6c8a91e35be055b1af5616fad1fe8fbb183
B0rj4 <borjita89@yahoo.es>
2026-03-11 15:48:36 -0500

    first commit

---
+++ UI/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Footzy',
+  webDir: 'www'
+};
+
+export default config;
