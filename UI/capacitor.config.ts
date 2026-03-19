ed311950c9bb30d32b7394024b4a970d38f0f370
B0rj4 <borjita89@yahoo.es>
2026-03-19 11:56:20 -0500

    Update capacitor to v8

--- UI/capacitor.config.ts
+++ UI/capacitor.config.ts
@@ -3,7 +3,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Footzy',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
+  plugins: {
+    BackgroundRunner: {
+      label: 'com.capacitor.background.check',
+      src: 'runners/runner.js',
+      event: 'checkIn',
+      repeat: true,
+      interval: 30,
+      autoStart: true,
+    },
+  },
 };
 
 export default config;
